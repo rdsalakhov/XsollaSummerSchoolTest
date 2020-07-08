1d1b6160e6cde281269c10b7f5448182a86b613e
rdsalakhov <salakhov.roman12@gmail.com>
2020-07-08 22:46:41 +0500

    Created data model and EF context

---
+++ XsollaSummerSchoolTest/Models/NewsDataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace XsollaSummerSchoolTest.Models
+{
+    public class NewsDataContext : DbContext
+    {
+        public NewsDataContext() : base("NewsDataModelContainer")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<NewsDataContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ XsollaSummerSchoolTest/NewsDataModel.edmx.sql
@@ -0,0 +1,56 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 07/08/2020 22:34:19
+-- Generated from EDMX file: \\mac\Home\Desktop\projects\2019-2020\C#\XsollaSummerSchoolTest\XsollaSummerSchoolTest\NewsDataModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [XsollaSummerSchoolTest];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'NewsItemSet'
+CREATE TABLE [dbo].[NewsItemSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Headline] nvarchar(max)  NOT NULL,
+    [Body] nvarchar(max)  NOT NULL,
+    [RateSum] smallint  NOT NULL,
+    [RateCount] smallint  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'NewsItemSet'
+ALTER TABLE [dbo].[NewsItemSet]
+ADD CONSTRAINT [PK_NewsItemSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
