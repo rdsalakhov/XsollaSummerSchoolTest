d58681701d19a9ef3e9a8499d2be1a823df5e721
rdsalakhov <salakhov.roman12@gmail.com>
2020-07-08 22:26:09 +0500

    Добавьте файлы проекта.

---
+++ XsollaSummerSchoolTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace XsollaSummerSchoolTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ XsollaSummerSchoolTest/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace XsollaSummerSchoolTest.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
