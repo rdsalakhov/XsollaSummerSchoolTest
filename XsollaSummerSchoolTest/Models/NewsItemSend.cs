98bef9e106bf694e7318a63160ebaf91456c6c9e
rdsalakhov <salakhov.roman12@gmail.com>
2020-07-14 20:27:44 +0500

    Added supportive moodel class for sending news items

---
+++ XsollaSummerSchoolTest/Models/NewsItemSend.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XsollaSummerSchoolTest.Models
+{
+    public class NewsItemSend
+    {
+        public int Id { get; set; }
+        public string Headline { get; set; }
+        public string Body { get; set; }
+        public int TotalRate { get; set; }
+        public int RateCount { get; set; }
+
+        public static implicit operator NewsItemSend(NewsItem newsItem)
+        {
+            var totalRate = newsItem.Rate.Select(x => int.Parse(x.Mark.ToString())).Sum();
+            var rateCount = newsItem.Rate.Count();
+            NewsItemSend newsItemSend = new NewsItemSend
+            {
+                Id = newsItem.Id,
+                Headline = newsItem.Headline,
+                Body = newsItem.Body,
+                TotalRate = totalRate,
+                RateCount = rateCount
+            };
+
+            return newsItemSend;
+        }
+    }
+}
